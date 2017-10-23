990a32dd16ef1d9b1247ae1368ca0d967fc7e0b9
Janjira012 <s5830213012@phuket.psu.ac.th>
2017-10-23 21:25:35 +0700

    Revert "Upload"
    This reverts commit c19a718c15c24bbfa315f02f7791ddaad7623bc9.

--- js/cache.js
+++
@@ -1,5 +0,0 @@
-$(function() {
-    $.ajaxSetup({
-        cache: false
-    });
-});
--- js/preloader.js
+++
@@ -1,7 +0,0 @@
-$(window).on('load', function() {
-    $('#status').fadeOut();
-    $('#preloader').delay(350).fadeOut('slow');
-    $('body').delay(350).css({
-        'overflow': 'visible'
-    });
-})
